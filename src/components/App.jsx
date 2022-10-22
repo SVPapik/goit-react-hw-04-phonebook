f1371d6a100587f7d7bef65e075b72ff939acfa6
Vitaliy <fireflier87@gmail.com>
2022-10-22 14:20:06 +0300

    methods componentDidMount & DidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,6 +39,20 @@ class App extends Component {
     this.setState({ filter: e.currentTarget.value });
   };
 
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { contacts, filter } = this.state;
     const visibleContacts = contacts.filter(contact =>
